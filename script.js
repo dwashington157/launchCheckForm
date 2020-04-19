396334c8356013835f7467017d9fee12eb91d479
Limit.Less <vegaveda777@gamil.com>
2020-04-19 15:19:46 -0500

    modified html js

--- script.js
+++ script.js
@@ -1,13 +1,72 @@
-// Write your JavaScript code here!
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+   form.addEventListener("submit", function(event) {
+      event.preventDefault();
+   let usernameInput = document.querySelector("input[name=pilotName]");
+   let cargo = document.querySelector("input[name=cargoMass]");
+   let fuel = document.querySelector("input[name=fuelLevel]");
+   let coPilot = document.querySelector("input[name=copilotName]");
+   let faultyItems = document.getElementById("faultyItems");
+   let launch = document.getElementById("launchStatus");
+   
+      document.getElementById("pilotStatus").innerHTML = `Pilot ${usernameInput.value}`;
+      document.getElementById("copilotStatus").innerHTML = `Co pilot ${coPilot.value}`;
+      if (usernameInput.value === "" ||cargo.value === "" || fuel.value === "" ||coPilot.value === "" ) {
+                 
+         alert("All input is Required!");
+      }else if(Number(usernameInput.value) || Number(coPilot.value)){
+         alert("Pilot and Co-Pilot must be names. no numbers or special")
+         
+      } else if ( isNaN(Number(cargo.value))||  isNaN(Number(fuel.value))) {
+         alert("Feild input requires numerical entry.");
+         
+      } else if ( fuel.value < 10000 || cargo.value > 10000){
+         faultyItems.style.visibility = "Visible" ;
+         launch.innerHTML = "Shuttle not ready for launch";
+         launch.style.color = "red";
+         if (fuel.value < 10000) {
+             document.getElementById("fuelStatus").innerHTML = "Fuel too low for launch";
+         }
+         if (cargo.value > 10000) { 
+            document.getElementById("cargoStatus").innerHTML = "There is too much mass for the shuttle to take off";
+         }        
+      
+      } else {
+         launch.innerHTML = "Shuttle is ready for launch";
+         launch.style.color = "green";
+      }
+      
+
+      
+   });
+
+  
+
+
+
+
+});
+
+window.addEventListener("load", function() {
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+       return response.json();
+   }).then(function(json) {
+       let div = document.getElementById("missionTarget");
+       div.innerHTML = `
+           <h2>Mission Destination </h2>
+           <ol>
+               <li>Name: ${json[3].name}</li>
+               <li>Diameter: ${json[3].diameter}</li>
+               <li>Star: ${json[3].star}</li>
+               <li>Distance from Earth: ${json[3].distance}</li>
+               <li>Number of Moons: ${json[3].moons}</li>
+           </ol>
+        <img src="https://www.nasa.gov/sites/default/files/images/587837main_Kepler16_transit_art2_full.jpg" />
+       `;
+   });
+});
+
+
+
